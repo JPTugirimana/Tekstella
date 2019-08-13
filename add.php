f972b9ed58f6d9e3298d7da66b49da7c83c15d4b
JP <tugirimana.jp@gmail.com>
2019-08-13 10:28:48 +0800

    Added Error persistance to the form

--- add.php
+++ add.php
@@ -1,35 +1,38 @@
 <?php 
 	
+	$email = $title = $details = '';
+	$errors = array('email'=>'','title'=>'','details'=>'');
+
 	if(isset($_POST['submit'])){
 		//echo htmlspecialchars($_POST['email']);
 
 		// Check Email
 		if(empty($_POST['email'])){
-			echo 'An email is required <br />';
+			$errors['email'] =  'An email is required <br />';
 		} else {
 			//echo htmlspecialchars($_POST['email']).'<br />';
 			$email = $_POST['email'];
 			if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-				echo 'Email is not valid!';
+				$errors['email'] = 'Email is not valid!';
 			}
 		}
 
 		if(empty($_POST['title'])){
-			echo 'A title is required <br />';
+			$errors['title'] = 'A title is required <br />';
 		} else {
 			//echo htmlspecialchars($_POST['title']);
 			$title = $_POST['title'];
 			if(!preg_match('/^[A-Za-z\s]+$/', $title)){
-				echo 'Must be letters and spaces only. ';
+				$errors['title'] = 'Must be letters and spaces only. ';
 			}
 		}
 
 		if(empty($_POST['details'])){
-			echo 'At least one detail is required <br />';
+			$errors['details'] = 'At least one detail is required <br />';
 		} else {
 			$details = $_POST['details'];
 			if(!preg_match('/^([A-Za-z\s]+)(,\s*[A-Za-z\s]*)*$/', $details)){
-				echo 'Must be comma-separated words! ';
+				$errors['details'] = 'Must be comma-separated words! ';
 			}
 		}
 	} // the end of POST check
@@ -45,11 +48,14 @@
 		<h4 class="center">Add a Curriculum</h4>
 		<form class="hite" action="" method="POST">
 			<label>Your Email:</label>
-			<input type="text" name="email">
+			<input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+			<div class="red-text"><?php echo $errors['email'] ?></div>
 			<label>Curriculum Title</label>
-			<input type="text" name="title">
+			<input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+			<div class="red-text"><?php echo $errors['title'] ?></div>
 			<label>Details (comma separated): </label>
-			<input type="text" name="details">
+			<input type="text" name="details" value="<?php echo htmlspecialchars($details) ?>">
+			<div class="red-text"><?php echo $errors['details'] ?></div>
 			<div class="center">
 				<input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
 			</div>
