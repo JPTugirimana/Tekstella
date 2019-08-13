940dee0f062983aafde5a92c6c137d9520007d7d
JP <tugirimana.jp@gmail.com>
2019-08-13 09:30:23 +0800

    Modified the footer copyright message

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 
 <footer class="section">
 	<div class="center grey-text">
-		Copyright 2019 Tekstella STEM Curriculum 
+		Copyright 2019. Tekstella STEM / Coding Curriculum and Lesson Plans.
 	</div>
 </footer>
 
