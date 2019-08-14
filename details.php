2937c3fa2a26591b8b0ca1edb5f41171df0c7772
JP <tugirimana.jp@gmail.com>
2019-08-14 12:33:06 +0800

    Added functionality delete single lesson from list/database

--- details.php
+++ details.php
@@ -3,6 +3,20 @@
 	// Connect to the database
 	include('config/db_connect.php'); 
 
+	if(isset($_POST['delete'])){
+		$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+		$sql = "DELETE FROM lessons WHERE id = $id_to_delete";
+
+		if(mysqli_query($conn, $sql)){
+			//success
+			header('Location: index.php');
+		} else {
+			echo 'query error: ' . mysqli_error($conn);
+		}
+
+	}
+
 	// check GET request id parameter
 	if(isset($_GET['id'])){
 		$id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -33,6 +47,12 @@
 			<p>Date : <?php echo date($lesson['created_at']); ?></p>
 			<h5>Details: </h5>
 			<p><?php echo htmlspecialchars($lesson['details']); ?></p>
+
+			<!--- Delete Form --->
+			<form action="details.php" method="POST">
+				<input type="hidden" name="id_to_delete" value="<?php echo $lesson['id'] ?>">
+				<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+			</form>
 		<?php else: ?>
 			<h5>No such lesson exists in our records.</h5>
 		<?php endif; ?>
