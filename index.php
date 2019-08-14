58536b28478834cc26082d21685071c91b8de0ac
JP <tugirimana.jp@gmail.com>
2019-08-14 10:57:08 +0800

    Added Fetching data from database and displaying them on home page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	}
 
 	// Query for all Lessons
-	$sql = 'SELECT title, details, id FROM lessons';
+	$sql = 'SELECT title, details, id FROM lessons ORDER BY created_at';
 
 	// Get query results
 	$result = mysqli_query($conn, $sql);
@@ -22,14 +22,43 @@
 	mysqli_free_result($result);
 	mysqli_close($conn);
 
-	print_r($lessons);
+	// Splitting items from the lists for better display in html
+	//explode(',',$lessons[0]['details']);
+
  ?>
 
 <!DOCTYPE html>
 <html>
 
+	<!-- Display the header -->
 	<?php include('templates/header.php'); ?>
 
+	<!--- Display Lesson Plans order by when they where created --->
+	<h4 class="center grey-text"> Lessons: </h4>
+	<div class="container">
+		<div class="row">
+			<?php foreach ($lessons as $lesson) { ?>
+				<div class="col s6 md3"> 
+					<div class="card z-depth-0"> 
+						<div class="card-content center">
+							<h5><?php echo htmlspecialchars($lesson['title']); ?></h5>
+							<ul>
+								<?php  foreach(explode(',',$lesson['details']) as $detail)  { ?>
+									<li><?php echo htmlspecialchars($detail)  ?></li>
+							<?php } ?>
+							</ul>
+						</div>
+						<div class="card-action center-align">
+						<a class="brand-text" href="#">more info</a>	
+						</div>
+					</div>
+				</div> 
+
+			<?php } ?>
+		</div>
+	</div>
+
+	<!--- Display the Footer -->
 	<?php include('templates/footer.php'); ?>
 
 </html>
